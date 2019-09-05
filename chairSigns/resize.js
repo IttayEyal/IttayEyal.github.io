3998187c5ab76f030e5ef75a0ac1f8c1c9ed9087
Ittay Eyal <stanga@gmail.com>
2019-09-06 00:15:17 +0300

    works but not on phone

--- chairSigns/resize.js
+++ chairSigns/resize.js
@@ -1,7 +1,30 @@
-var elemDiv = document.getElementById('a'),
-  elemInput = document.getElementById('b');
+var elemDiv = document.getElementById('shown'); 
+var elemInput = document.getElementById('hidden');
+var elemPlus = document.getElementById('plus');
+var elemMinus = document.getElementById('minus');
 
-elemDiv.onblur = function() {
+var counter = 10; 
+
+// elemPlus.onclick = function() {
+//     counter += 1; 
+//     elemDiv.innerHTML = counter; 
+//     fitSize(); 
+// };
+// 
+// elemMinus.onclick = function() { 
+//     counter -= 1; 
+//     elemDiv.innerHTML = counter; 
+//     fitSize(); 
+// };
+
+function plus() {
+    counter += 1; 
+    elemDiv.innerHTML = counter; 
+    fitSize(); 
+}
+
+// elemDiv.oninput = function() {
+function fitSize() {
   elemInput.value = elemDiv.innerHTML;
   
   clientWidth = document.documentElement.clientWidth; 
@@ -13,8 +36,8 @@ elemDiv.onblur = function() {
   fontSize = 10; 
   while (divWidth < clientWidth && divHeight < clientHeight) {
     fontSize *= 2; 
-    document.getElementById("a").style.fontSize = fontSize + "px";
-    document.getElementById("b").style.fontSize = fontSize + "px";  
+    document.getElementById("shown").style.fontSize = fontSize + "px";
+    document.getElementById("hidden").style.fontSize = fontSize + "px";  
     divWidth = elemDiv.clientWidth; 
     divHeight = elemDiv.clientHeight; 
   } 
@@ -22,22 +45,21 @@ elemDiv.onblur = function() {
 // Shrink: 
   while (divWidth > clientWidth || divHeight > clientHeight) {
     fontSize *= 0.9; 
-    document.getElementById("a").style.fontSize = fontSize + "px";
-    document.getElementById("b").style.fontSize = fontSize + "px";  
+    document.getElementById("shown").style.fontSize = fontSize + "px";
+    document.getElementById("hidden").style.fontSize = fontSize + "px";  
     divWidth = elemDiv.clientWidth; 
     divHeight = elemDiv.clientHeight; 
   } 
-//  document.getElementById("a").innerHTML = 5+1 + "px";
-//  document.getElementById("b").innerHTML = "stam";
+//  document.getElementById("shown").innerHTML = 5+1 + "px";
+//  document.getElementById("hidden").innerHTML = "stam";
 }
 
-var buttonBlink = document.createElement("Button");
-buttonBlink.innerHTML = "blink";
-buttonBlink.style = "bottom:0;right:0;position:absolute;z-index: 9999"
-buttonBlink.onclick = function() {doBlink()};
-document.body.appendChild(buttonBlink);
-
+// var buttonBlink = document.createElement("Button");
+// buttonBlink.innerHTML = "blink";
+// buttonBlink.style = "bottom:0;right:0;position:absolute;z-index: 9999"
+// buttonBlink.onclick = function() {doBlink()};
+// document.body.appendChild(buttonBlink);
+// 
 function doBlink() {
-    
-  document.body.style.backgroundColor = "gray";
+    document.body.style.backgroundColor = "gray";
 }
